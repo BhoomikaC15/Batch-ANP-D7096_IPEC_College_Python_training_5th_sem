620de7081a7dc22e199023dd8654c4b095be4605
Bhoomika Choudhury <choudhurybhoomika@gmail.com>
2026-06-27 20:34:10 +0530

    Add Greater_number

--- Class_Work/Day3_Selection_statement/Greater_number.py
+++ Class_Work/Day3_Selection_statement/Greater_number.py
@@ -11,3 +11,13 @@ The greater number is: 20
 ----------------------------------------------------------
 --------------------------------------------Coding----------------------------------------
 '''
+first_number = float(input("Enter first number: "))
+second_number = float(input("Enter second number: "))
+print("-------------------------------------------------")
+if first_number>second_number:
+    print("The greater number is: ", first_number)
+elif second_number>first_number:
+    print("The greater number is : ", second_number)
+else:
+    print("The number are equal. No greater number")
+print("-------------------------------------------------")
