12c1fdc1b1c16c504fc01388ef9df72927bd5c4c
Bhoomika Choudhury <choudhurybhoomika@gmail.com>
2026-06-30 09:23:24 +0530

    add classroom seating arrangement

---
+++ Class_Work/day1_arithimatic_operator_24th_july/classroom_seating_arrangement.py
@@ -0,0 +1,30 @@
+'''
+--------------------------------------Classroom Seating Arrangement------------------------------------
+Scenario:
+A school wants to arrange students into equal rows.
+Problem Statement:
+Write a Python program to determine how many complete rows can be formed.
+Input:
+• Total Students
+• Students per Row
+Output:
+• Number of Complete Rows
+--------------------------------------------------------------
+Sample Input:
+Enter Total Students: 25
+Enter Students per Row: 4
+--------------------------------------------------------------
+Sample Output:
+Number of Complete Rows: 6
+'''
+total_students = int(input("Enter Total Students: "))
+students_per_row = int(input("Enter Students per Row: "))
+#-------------Validating the inputs----------------
+if total_students < 0 or students_per_row <= 0:
+    exit("Enter valid values.")
+#--------------------------------------------------
+complete_rows = total_students // students_per_row
+print("--------------------------------------------------------------")
+print("Number of Complete Rows:", complete_rows)
+print("--------------------------------------------------------------")
+#---------------------------------------------------------------------------------------------------
